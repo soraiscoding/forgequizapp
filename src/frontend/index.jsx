f11f6c3763b264f8d72beecc1c2731d389ca08b3
Richard Zhou <richardzhou@Richards-MacBook-Air.local>
2026-06-21 21:06:14 +1000

    added an end screen, replay functionality and re made the node_modules and package-lock file

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -13,9 +13,14 @@ const App = () => {
   const [explanation, setExplanation] = useState('placeholder explanation')
   const [showResult, setShowResult] = useState(false);
 
+  // when we reach the final question reachedEnd will lead to the end screen being displayed
+  const [reachedEnd, setReachedEnd] = useState(false);
+  const [score, setScore] = useState(0);
+
   const onClickHandler = (isCorrect) => {
     if (isCorrect) {
       setExplanation('You got it right!');
+      setScore(score + 1);
     } else {
       setExplanation('Incorrect, the correct answer is ' + `${QuestionSet[activeQuestion].correctAnswer}`);
     }
@@ -27,17 +32,38 @@ const App = () => {
     if (activeQuestion + 1 < QuestionSet.length) {
       setActiveQuestion(activeQuestion + 1);
       setShowResult(false);
+    } else {
+      setReachedEnd(true); // we reached the last question so reachedend is set to true
     }
+  }
+
+  // for restarting the game
+  const onClickReplay = () => {
+    setActiveQuestion(0);
+    setShowResult(false);
+    setScore(0);
+    setExplanation('');
+    setReachedEnd(false);
   }
 
   const { question, options, image } = QuestionSet[activeQuestion];
 
   return (
     <>
+      {/* If reachedEnd is true we show the end screen, otherwise we just move on to the next question */}
+      { reachedEnd ? (
+        <Stack space="space.200" alignInline="center">
+          <Heading as="h1">Final score: {score} out of {QuestionSet.length} </Heading>
+          <Image src={"https://media.giphy.com/media/XROOE9NApITmCgF6dZ/giphy.gif"} alt='High-five' size = "small"/>
+          <Button appearance="primary" onClick={onClickReplay}>
+              Replay
+          </Button>
+        </Stack>
+      ) : (
       <Stack space="space.200" alignInline="center">
         <Heading as="h1">{question}</Heading>
 
-        {/* if there's no image then you get the mario block gif */}
+        {/* if there's no image (meaning no questions, since we try extract an image from a question) then you get the mario block gif */}
         <Image src={image ? image : "https://media.giphy.com/media/xUOxfjsW9fWPqEWouI/giphy.gif"} alt="Founders" size="xsmall" />
         <Inline space="space.200" alignBlock="center" alignInline="center">
           <Stack space="space.200" grow="hug">
@@ -57,10 +83,17 @@ const App = () => {
             </Button>
           </Stack>
         </Inline>
+
+        {/* you got it right! or incorrect, the correct answer is "" */}
         <Text>{showResult ? explanation : null}</Text>
+        
+        {/* onClick we run onClickNext, isDisabled is for whether the button is greyed out and uninteractable or not */}
         <Button appearance='default' onClick={onClickNext} isDisabled={showResult ? false : true}>{ activeQuestion == QuestionSet.length-1 ? 'Finish' : 'Next Question'}</Button>
+        
+        {/* Question 2 of 3 (for example) */}
         <Text>Question {activeQuestion + 1} of {QuestionSet.length}</Text>
-      </Stack>
+      </Stack> )
+      }
     </>
   );
 };
